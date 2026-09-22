90c33fc52744c92e22da89253060310de228e336
shuklaji7058 <shuklaanish040@gmail.com>
2026-09-22 21:25:25 +0530

    java

--- CmdLineArgs.java
+++ CmdLineArgs.java
@@ -1,2 +1,21 @@
 class CmdLineArgs
-{}
+{
+	public static void main(String[] args)
+	{
+		//command line arguments
+		/*for(String fan : args)
+		  {
+			System.out.println(fan);
+		  }*/
+		
+		System.out.println(args);
+		System.out.println(args.length);
+		System.out.println("using s.o.pln- " + args[0]);
+		
+		for(int i=0; i<args.length;i++)
+		{
+			System.out.println(args[i]);
+		}
+		
+	}
+}
